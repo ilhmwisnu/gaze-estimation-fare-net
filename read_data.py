463574255eebc5824b9ac7c33f2dc58829cc18e1
ilhmwisnu <ilhamwisnu42@gmail.com>
2024-06-16 19:47:06 +0700

    feat: adjust read data func

--- read_data.py
+++ read_data.py
@@ -4,14 +4,14 @@ import cv2 as cv
 import numpy as np
 
 # Function to load images and annotations from a subject directory
-def load_subject_data(subject_dir):
+def load_subject_data(subject_dir, start=0, end=1000):
     l_eye_imgs = []
     r_eye_imgs = []
     face_imgs = []
     labels = []
     
     days = [day_dir for day_dir in os.listdir(subject_dir) if day_dir.startswith("day")]
-    count = 0
+    index = 0
     
     for day in days:
         day_path = os.path.join(subject_dir, day)
@@ -26,10 +26,12 @@ def load_subject_data(subject_dir):
             # Load images and annotations
             for ann in annotations:
                 
-                if count == 1000:
+                if index < start :
+                    index += 1
+                    continue
+                
+                if index == end:
                     break
-
-                count +=1
 
                 image_name = ann[0]  
                 label = ann[1:]
@@ -38,19 +40,22 @@ def load_subject_data(subject_dir):
                 
                 r_eye_img = cv.imread(f"{day_path}/right_eye/{image_name}",cv.IMREAD_GRAYSCALE) / 255.0
                 
-                face_img = cv.imread(f"{day_path}/face/{image_name}") / 255.0
+                face_img = cv.imread(f"{day_path}/face/{image_name}")
+                face_img = cv.cvtColor(face_img,cv.COLOR_BGR2RGB)  / 255.0
                 
                 l_eye_imgs.append(l_eye_img)
                 r_eye_imgs.append(r_eye_img)
                 face_imgs.append(face_img)
                 labels.append(label)
-
-
                 
+                index +=1
+
+    print("Rows: " + str(len(labels)))
+
     return np.array(l_eye_imgs),np.array(r_eye_imgs),np.array(face_imgs),np.array(labels, dtype=float) 
 
 # Function to load the entire dataset
-def load_dataset(dataset_type="original"):
+def load_dataset(dataset_type="original", start=0, end=1000):
     path = f"data_subset/{dataset_type}" 
 
     
@@ -66,7 +71,7 @@ def load_dataset(dataset_type="original"):
         print(f"Reading => {subject_path}")
 
         if os.path.isdir(subject_path):
-            l_eye_imgs, r_eye_imgs, face_imgs, labels = load_subject_data(subject_path)
+            l_eye_imgs, r_eye_imgs, face_imgs, labels = load_subject_data(subject_path, start=start, end=end)
             
             l_eye_list.extend(l_eye_imgs)
             r_eye_list.extend(r_eye_imgs)
